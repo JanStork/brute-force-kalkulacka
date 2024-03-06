0b173e6f459defc66c32510db7ec27f94729a792
JanStork <jan.stork@protonmail.com>
2024-03-06 06:08:12 +0100

    I just have to finish the function

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # brute-force-kalkulacka
 
 Brute force kalkulačka zadaná jako domácí úkol z kybernetické bezpečnosti
+
 Python, CustomTkinter
+
+Uděláno s pomocí Kofoly & Spotify
--- bruteforcekalk.py
+++ bruteforcekalk.py
@@ -4,33 +4,54 @@ customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
-root.geometry("1280x760")
+root.geometry("800x650")
 
 # function
-
-def login():
-    print("Hello world!")
-
+# def calculation(passwordLength, charLength):
 
 # GUI
-
-
-
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = customtkinter.CTkLabel(master=frame, text="Login system")
+label = customtkinter.CTkLabel(master=frame, text="Brute force kalkulačka")
 label.pack(pady=12, padx=10)
 
-entry1 = customtkinter.CTkEntry(master = frame, placeholder_text="Username")
-entry1.pack(pady=12, padx=10)
-entry2 = customtkinter.CTkEntry(master = frame, placeholder_text="Password", show="*")
-entry2.pack(pady=12, padx=10)
+# Checkboxes for options
+checkbox1 = customtkinter.CTkCheckBox(master=frame, text="Malá písmena (a-z)")
+checkbox2 = customtkinter.CTkCheckBox(master=frame, text="Velká písmena (A-Z)")
+checkbox3 = customtkinter.CTkCheckBox(master=frame, text="Číslice (0-9)")
+checkbox4 = customtkinter.CTkCheckBox(master=frame, text="Speciální znaky (*!@)")
 
-button = customtkinter.CTkButton(master = frame, text="Login", command=login)
-button.pack(pady = 12, padx=10)
+checkbox1.pack(anchor='w', pady=2, padx=10)
+checkbox2.pack(anchor='w', pady=2, padx=10)
+checkbox3.pack(anchor='w', pady=2, padx=10)
+checkbox4.pack(anchor='w', pady=2, padx=10)
 
-checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember me")
-checkbox.pack(pady=12, padx=10)
+# Entry textbox
+entryLabelNChars = customtkinter.CTkLabel(master=frame, text="Počet znaků hesla:")
+entryLabelNChars.pack(pady=5, padx=10)
+
+entryTextTriesS = customtkinter.CTkEntry(master=frame)
+entryTextTriesS.pack(pady=2.5, padx=10)
+
+entryLabelTriesS = customtkinter.CTkLabel(master=frame, text="Počet pokusů/s:")
+entryLabelTriesS.pack(pady=5, padx=10)
+
+entryTextTriesS = customtkinter.CTkEntry(master=frame)
+entryTextTriesS.pack(pady=2.5, padx=10)
+
+# Button
+button = customtkinter.CTkButton(master=frame, text="Vypočítej", command=calculation)
+button.pack(pady=10)
+
+# Labels
+labelLenghtCalc = customtkinter.CTkLabel(master=frame, text="Brute force attack bude trvat")
+labelLenghtCalc.pack(padx=10)
+
+labelNOperations = customtkinter.CTkLabel(master=frame, text="Proběhne přes XXX pokusů")
+labelNOperations.pack(padx=10)
+
+
+
 
 root.mainloop()
